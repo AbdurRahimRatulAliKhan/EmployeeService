5563f1bdff439c2f2da0ef37548689dbce33d667
Ratul Ali <rahim498617@gmail.com>
2022-04-16 17:27:16 +0600

    Add project files.

---
+++ EmployeeService/Controllers/StudentsController.cs
@@ -0,0 +1,43 @@
+﻿using EmployeeService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        static List<Student> students = new List<Student>()
+        {
+            new Student() { Id = 1, Name = "Tonmoy"},
+            new Student() { Id = 2, Name = "Tinny"},
+            new Student() { Id = 3, Name = "Tony"}
+        };
+
+        
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return students;
+        }
+
+        [HttpGet]
+        public Student Get(int id)
+        {
+            return students.FirstOrDefault(s => s.Id == id);
+        }
+
+        [HttpGet]
+        [Route("api/students/{id}/courses")]
+        public IEnumerable<string>GetStudentCourses(int id)
+        {
+            if (id == 1)
+                return new List<string>() { "C#", "ASP.NET", "SQL Server" };
+            else if (id == 2)
+                return new List<string>() { "ASP.NET Web API", "C++", "SQL Server" };
+            else
+                return new List<string>() { "Bootstrap", "jQuery", "AngularJs" }; 
+        }
+    }
+}
