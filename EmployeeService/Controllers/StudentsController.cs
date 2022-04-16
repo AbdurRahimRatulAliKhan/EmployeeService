153db8f03d25f2d02a4cab76e145526ea32547dc
Ratul Ali <rahim498617@gmail.com>
2022-04-17 01:37:47 +0600

    Updated APIs v2

--- EmployeeService/Controllers/StudentsController.cs
+++ EmployeeService/Controllers/StudentsController.cs
@@ -4,8 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeService.Controllers
 {
-    //[Route("api/[controller]")]
-    //[ApiController]
+    [Route("api/[controller]")]
+    [ApiController]
     public class StudentsController : ControllerBase
     {
         static List<Student> students = new List<Student>()
@@ -15,23 +15,46 @@ namespace EmployeeService.Controllers
             new Student() { Id = 3, Name = "Tony"}
         };
 
+        [HttpGet]
+        [Route("~/api/teachers")]
+        public IEnumerable<Teacher> GetTeachers()
+        {
+            List<Teacher> teachers = new List<Teacher>()
+            {
+                new Teacher() { Id = 1, Name ="Tomy"},
+                new Teacher() { Id = 2, Name ="Moni"},
+                new Teacher(){ Id = 3, Name ="Niloy"}
+            };
+            return teachers;
+        }
         
         [HttpGet]
-        [Route("api/students")]
+        [Route("")]
         public IEnumerable<Student> Get()
         {
             return students;
         }
 
         [HttpGet]
-        [Route("api/students/{id}")]
+        //[Route("api/students/{id}")]
+        //[Route("{id:int:min(1):max(3)}")]
+        [Route("{id:int:range(1, 3)}")]
         public Student Get(int id)
         {
             return students.FirstOrDefault(s => s.Id == id);
         }
 
         [HttpGet]
-        [Route("api/students/{id}/courses")]
+        //[Route("api/students/{id}")]
+        [Route("{name:alpha}")]
+        public Student Get(string name)
+        {
+            return students.FirstOrDefault(s => s.Name.ToLower() == name.ToLower());
+        }
+
+        [HttpGet]
+        //[Route("api/students/{id}/courses")]
+        [Route("{id}/courses")]
         public IEnumerable<string>GetStudentCourses(int id)
         {
             if (id == 1)
